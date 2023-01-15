10a4bcd9304fbd8a9662c21afe8cd4b25b2b233e
doctorvtj83 <15706728+doctorvtj83@users.noreply.github.com>
2023-01-15 17:32:23 +0100

    retrieving image metadata

--- media_renamer.py
+++ media_renamer.py
@@ -1,8 +1,36 @@
 import argparse
+import os
+from PIL import Image
+from PIL.ExifTags import TAGS
+from pillow_heif import HeifImagePlugin
 
 class Renamer:
     def __init__(self,path):
         self.path = path
+
+    def get_files(self):
+        self.files = [ {'Name': el, 'Type' : os.path.splitext(el)[1].lower()} for el in os.listdir(self.path)]
+
+    def get_labeled_exif(self,exif):
+        return {
+        TAGS.get(key, key): value
+        for key, value in exif.items()
+    }
+
+    def retrieve_datetime_from_meta(self):
+        for file in self.files:
+            file["CreateTimestamp"] = False
+            
+            if file['Type'] in ['.jpg','.jpeg','.png','.heic']:
+                fp = self.path + '/' +file['Name']
+                with Image.open(fp) as img:
+                    exif = img.getexif()
+                    if exif:
+                        labeled_exif = self.get_labeled_exif(exif)
+                        if 'DateTime' in labeled_exif:
+                            file["CreateTimestamp"] = labeled_exif['DateTime']
+
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -15,3 +43,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     R = Renamer(args.path)
+    R.get_files()
+    R.retrieve_datetime_from_meta()
+
+    print(R.files)
